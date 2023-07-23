98a81c6baebfaea0aff021d45fd418bb05fa19f7
Ina <matz1208@students.cs.ubc.ca>
2023-07-22 21:20:52 -0700

    Added methods to Tile.java

--- src/main/java/com/example/demo/model/Tile.java
+++ src/main/java/com/example/demo/model/Tile.java
@@ -1,4 +1,17 @@
 package com.example.demo.model;
 
 public class Tile {
+    private int num;
+
+    public Tile() {
+        num = 1;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int newNum) {
+        num = newNum;
+    }
 }
