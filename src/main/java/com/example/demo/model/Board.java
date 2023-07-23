4cfefd124b3c5e2c3623cb08e68afa7a4766e49c
Ina Lee <inalee1208@gmail.com>
2023-07-22 18:11:47 -0700

    Added methods to Driver.java

--- src/main/java/com/example/demo/model/Board.java
+++ src/main/java/com/example/demo/model/Board.java
@@ -1,4 +1,9 @@
 package com.example.demo.model;
 
 public class Board {
+    int[][] grid;
+
+    public Board() {
+        grid = new int[4][4];
+    }
 }
---
+++ src/main/java/com/example/demo/ui/Driver.java
@@ -0,0 +1,34 @@
+package com.example.demo.ui;
+
+import com.example.demo.ui.tools.BoardBuilder;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * The driver of the 1024 game application
+ *
+ * @author Ina Lee
+ * @version 2023-07-22
+ */
+
+public class Driver extends Application {
+
+    /**
+     * Main method that launches the game application.
+     *
+     * @param args isn't used.
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+
+    /**
+     * JavaFX start method.
+     *
+     * @param stage is a stage to display.
+     */
+    @Override
+    public void start(Stage stage) {
+        BoardBuilder.buildStage(stage);
+    }
+}
