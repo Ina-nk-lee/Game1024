9b5dcefb27ef2b2d7725a8284d58ba0cc69a523d
Ina Lee <inalee1208@gmail.com>
2023-07-22 17:45:11 -0700

    Restructured classes

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.demo.ui to javafx.fxml;
+    exports com.example.demo.ui;
+    exports com.example.demo.ui.tools;
+    opens com.example.demo.ui.tools to javafx.fxml;
+}
