30375b84be7b1a1c46bb47ed22bccca23fb4ac7c
Ina Lee <inalee1208@gmail.com>
2023-07-23 21:02:09 -0700

    Edited the pushLeft method to work. Added clearBoard method as well.

--- src/test/java/com/example/demo/model/BoardTest.java
+++ src/test/java/com/example/demo/model/BoardTest.java
@@ -7,11 +7,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
     Board testBoard;
+    Tile tileA;
+    Tile tileB;
     int SIZE = 4;
 
     @BeforeEach
     void runBefore() {
         testBoard = new Board();
+        tileA = new Tile();
+        tileB = new Tile();
     }
 
     @Test
@@ -24,11 +28,70 @@ public class BoardTest {
 
     @Test
     void testInitGame() {
+        testBoard.initGame();
+        Tile[][] testGrid = testBoard.getGrid();
         int count = 0;
+
         for(int row = 0; row < SIZE; row++) {
             for(int col = 0; col < SIZE; col++) {
-                //if()
+                if(testGrid[row][col].getNum() == 1) {
+                    count++;
+                }
             }
         }
+
+        assertEquals(2, count);
+    }
+
+    @Test
+    void testGetTile() {
+        testBoard.getTile(0, 0).setNum(1);
+        testBoard.getTile(0, 1).setNum(2);
+        assertEquals("1 2 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n", testBoard.toString());
+    }
+
+    @Test
+    void testPushLeftWhenLeftIsEmpty() {
+        testBoard.getTile(0, 1).setNum(1);
+        testBoard.getTile(0, 2).setNum(2);
+        testBoard.pushLeft();
+        assertEquals("1 2 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n", testBoard.toString());
+    }
+
+    @Test
+    void testPushLeftWhenMiddleIsEmpty() {
+        testBoard.getTile(0, 0).setNum(1);
+        testBoard.getTile(0, 3).setNum(2);
+        testBoard.pushLeft();
+        assertEquals("1 2 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n", testBoard.toString());
+
+        testBoard.clearBoard();
+        testBoard.getTile(0, 1).setNum(1);
+        testBoard.getTile(0, 3).setNum(2);
+        testBoard.pushLeft();
+        assertEquals("1 2 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n", testBoard.toString());
+    }
+
+    @Test
+    void testPushLeftWhenRightIsEmpty() {
+        testBoard.getTile(0, 1).setNum(1);
+        testBoard.getTile(0, 3).setNum(2);
+        testBoard.pushLeft();
+        assertEquals("1 2 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n", testBoard.toString());
     }
 }
