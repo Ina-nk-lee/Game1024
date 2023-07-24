acb0505d5cba2285ff41b2bc1ed781d0ee2918c9
Ina Lee <inalee1208@gmail.com>
2023-07-23 21:13:57 -0700

    Changed Tile to int

--- src/main/java/com/example/demo/model/Board.java
+++ src/main/java/com/example/demo/model/Board.java
@@ -7,32 +7,32 @@ import java.util.Random;
  *
  */
 public class Board {
-    Tile[][] grid;
-    int SIZE = 4;
+    int[][] grid;
+    int BOARD_SIZE = 4;
 
     /**
-     * A Board constructor that creates a 4x4 Tile grid.
+     * A Board constructor that creates a 4x4 int grid.
      */
     public Board() {
-        grid = new Tile[SIZE][SIZE];
+        grid = new int[BOARD_SIZE][BOARD_SIZE];
         this.clearBoard();
     }
 
     /**
-     * A getter for a Tile in the Board.
-     * @param row of the tile in the Board. 0 is the first row.
-     * @param col of the tile in the Board. 0 is the first column.
-     * @return a Tile with the given row and column.
+     * A getter for a int in the Board.
+     * @param row of the int in the Board. 0 is the first row.
+     * @param col of the int in the Board. 0 is the first column.
+     * @return a int with the given row and column.
      */
-    protected Tile getTile(int row, int col) {
+    protected int getint(int row, int col) {
         return grid[row][col];
     }
 
     /**
-     * A setter for a Tile's number.
+     * A setter for a int's number.
      * @param value of the number.
-     * @param row of the tile in the Board. 0 is the first row.
-     * @param col of the tile in the Board. 0 is the first column.
+     * @param row of the int in the Board. 0 is the first row.
+     * @param col of the int in the Board. 0 is the first column.
      */
     protected void setNum(int value, int row, int col) {
         grid[row][col].setNum(value);
@@ -40,37 +40,37 @@ public class Board {
 
     /**
      * A method that initializes a game.
-     * It picks two Tiles on the board randomly and changes their numbers to 1.
+     * It picks two ints on the board randomly and changes their numbers to 1.
      */
     public void initGame() {
         Random random = new Random();
 
-        int randomRowA = random.nextInt(SIZE);
-        int randomColA = random.nextInt(SIZE);
+        int randomRowA = random.nextInt(BOARD_SIZE);
+        int randomColA = random.nextInt(BOARD_SIZE);
 
         grid[randomRowA][randomColA].setNum(1);
 
-        int randomRowB = random.nextInt(SIZE);
-        int randomColB = random.nextInt(SIZE);
+        int randomRowB = random.nextInt(BOARD_SIZE);
+        int randomColB = random.nextInt(BOARD_SIZE);
 
         while(randomRowA == randomRowB && randomColA == randomColB) {
-            randomRowB = random.nextInt(SIZE);
-            randomColB = random.nextInt(SIZE);
+            randomRowB = random.nextInt(BOARD_SIZE);
+            randomColB = random.nextInt(BOARD_SIZE);
         }
 
         grid[randomRowB][randomColB].setNum(1);
     }
 
     protected void clearBoard() {
-        for(int row = 0; row < SIZE; row++) {
-            for(int col = 0; col < SIZE; col++) {
-                grid[row][col] = new Tile();
+        for(int row = 0; row < BOARD_SIZE; row++) {
+            for(int col = 0; col < BOARD_SIZE; col++) {
+                grid[row][col] = new int();
             }
         }
     }
 
     public void pushLeft() {
-        for(int row = 0; row < SIZE; row++) {
+        for(int row = 0; row < BOARD_SIZE; row++) {
             if(!isRowAllZero(row)) {
                 moveLeft(row);
             }
@@ -78,9 +78,9 @@ public class Board {
     }
 
     public void moveLeft(int row) {
-        for(int i = 0; i < SIZE - 1; i++) {
-            for(int col = i; col < SIZE - 1; col++) {
-                Tile curr = grid[row][col];
+        for(int i = 0; i < BOARD_SIZE - 1; i++) {
+            for(int col = i; col < BOARD_SIZE - 1; col++) {
+                int curr = grid[row][col];
                 if(curr.getNum() == 0) {
                     swap(row, col, row, col + 1);
                 }
@@ -88,25 +88,21 @@ public class Board {
         }
     }
 
-    // 1 0 0 2
-    // 1 0 0 2
-    // 1 0 2 0
-
     private void swap(int rowA, int colA, int rowB, int colB) {
-        Tile temp = grid[rowA][colA];
+        int temp = grid[rowA][colA];
         grid[rowA][colA] = grid[rowB][colB];
         grid[rowB][colB] = temp;
     }
 
     /**
-     * Checks whether all the Tiles in a row  zero.
+     * Checks whether all the ints in a row  zero.
      * @param row to be checked.
-     * @return true if all the Tiles are zero, false if not.
+     * @return true if all the ints are zero, false if not.
      */
     private boolean isRowAllZero(int row) {
         boolean result = true;
-        for(int col = 0; col < SIZE && result; col++) {
-            Tile curr = grid[row][col];
+        for(int col = 0; col < BOARD_SIZE && result; col++) {
+            int curr = grid[row][col];
             if(curr.getNum() != 0) {
                 result = false;
             }
@@ -114,11 +110,11 @@ public class Board {
         return result;
     }
 
-    private void merge(Tile tileA, Tile tileB) {
+    private void merge(int intA, int intB) {
 
     }
 
-    protected Tile[][] getGrid() {
+    protected int[][] getGrid() {
         return grid;
     }
 
@@ -127,8 +123,8 @@ public class Board {
      */
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for(int row = 0; row < SIZE; row++) {
-            for(int col = 0; col < SIZE; col++) {
+        for(int row = 0; row < BOARD_SIZE; row++) {
+            for(int col = 0; col < BOARD_SIZE; col++) {
                 result.append(grid[row][col].getNum()).append(" ");
             }
             result.append("\n");
