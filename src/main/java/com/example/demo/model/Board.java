2438229053ab879d023440cb066bc00aac8a8c4e
Ina <matz1208@students.cs.ubc.ca>
2023-07-24 00:45:09 -0700

    Updated moveLeft method of Board.java and changed Tiles into integers.

--- src/main/java/com/example/demo/model/Board.java
+++ src/main/java/com/example/demo/model/Board.java
@@ -15,27 +15,26 @@ public class Board {
      */
     public Board() {
         grid = new int[BOARD_SIZE][BOARD_SIZE];
-        this.clearBoard();
     }
 
     /**
-     * A getter for a int in the Board.
+     * A getter for number on the Board.
      * @param row of the int in the Board. 0 is the first row.
      * @param col of the int in the Board. 0 is the first column.
      * @return a int with the given row and column.
      */
-    protected int getint(int row, int col) {
+    protected int getNum(int row, int col) {
         return grid[row][col];
     }
 
     /**
-     * A setter for a int's number.
+     * A setter for a number on the Board.
      * @param value of the number.
      * @param row of the int in the Board. 0 is the first row.
      * @param col of the int in the Board. 0 is the first column.
      */
     protected void setNum(int value, int row, int col) {
-        grid[row][col].setNum(value);
+        grid[row][col] = value;
     }
 
     /**
@@ -48,7 +47,7 @@ public class Board {
         int randomRowA = random.nextInt(BOARD_SIZE);
         int randomColA = random.nextInt(BOARD_SIZE);
 
-        grid[randomRowA][randomColA].setNum(1);
+        grid[randomRowA][randomColA] = 1;
 
         int randomRowB = random.nextInt(BOARD_SIZE);
         int randomColB = random.nextInt(BOARD_SIZE);
@@ -58,17 +57,23 @@ public class Board {
             randomColB = random.nextInt(BOARD_SIZE);
         }
 
-        grid[randomRowB][randomColB].setNum(1);
+        grid[randomRowB][randomColB] = 1;
     }
 
-    protected void clearBoard() {
+    /**
+     * Initializes the Board with a zero in all tiles.
+     */
+    protected void initBoard() {
         for(int row = 0; row < BOARD_SIZE; row++) {
             for(int col = 0; col < BOARD_SIZE; col++) {
-                grid[row][col] = new int();
+                grid[row][col] = 0;
             }
         }
     }
 
+    /**
+     * Pushes the Board to left.
+     */
     public void pushLeft() {
         for(int row = 0; row < BOARD_SIZE; row++) {
             if(!isRowAllZero(row)) {
@@ -77,37 +82,43 @@ public class Board {
         }
     }
 
+    /**
+     * Moves all non-zero numbers in a given row to the left.
+     * @param row to move numbers
+     */
     public void moveLeft(int row) {
-        for(int i = 0; i < BOARD_SIZE - 1; i++) {
-            for(int col = i; col < BOARD_SIZE - 1; col++) {
-                int curr = grid[row][col];
-                if(curr.getNum() == 0) {
-                    swap(row, col, row, col + 1);
-                }
+        int[] newRow = new int[BOARD_SIZE];
+        int j = 0;
+
+        for(int col = 0; col < BOARD_SIZE; col++) {
+            if(grid[row][col] != 0) {
+                newRow[j] = grid[row][col];
+                j++;
             }
         }
+
+        grid[row] = newRow;
+    }
+
+    /**
+     * Checks whether how many numbers in a row are zero.
+     * @param row to be checked.
+     * @return the number of zeros in the row.
+     */
+    private boolean isRowAllZero(int row) {
+        boolean result = true;
+        for(int i = 0; i < BOARD_SIZE; i++) {
+            if(grid[row][i] != 0) {
+                result = false;
+            }
+        }
+        return result;
     }
 
     private void swap(int rowA, int colA, int rowB, int colB) {
         int temp = grid[rowA][colA];
         grid[rowA][colA] = grid[rowB][colB];
         grid[rowB][colB] = temp;
-    }
-
-    /**
-     * Checks whether all the ints in a row  zero.
-     * @param row to be checked.
-     * @return true if all the ints are zero, false if not.
-     */
-    private boolean isRowAllZero(int row) {
-        boolean result = true;
-        for(int col = 0; col < BOARD_SIZE && result; col++) {
-            int curr = grid[row][col];
-            if(curr.getNum() != 0) {
-                result = false;
-            }
-        }
-        return result;
     }
 
     private void merge(int intA, int intB) {
@@ -125,7 +136,7 @@ public class Board {
         StringBuilder result = new StringBuilder();
         for(int row = 0; row < BOARD_SIZE; row++) {
             for(int col = 0; col < BOARD_SIZE; col++) {
-                result.append(grid[row][col].getNum()).append(" ");
+                result.append(grid[row][col]).append(" ");
             }
             result.append("\n");
         }
--- src/test/java/com/example/demo/model/BoardTest.java
+++ src/test/java/com/example/demo/model/BoardTest.java
@@ -7,15 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BoardTest {
     Board testBoard;
-    Tile tileA;
-    Tile tileB;
     int SIZE = 4;
 
     @BeforeEach
     void runBefore() {
         testBoard = new Board();
-        tileA = new Tile();
-        tileB = new Tile();
     }
 
     @Test
@@ -29,12 +25,12 @@ public class BoardTest {
     @Test
     void testInitGame() {
         testBoard.initGame();
-        Tile[][] testGrid = testBoard.getGrid();
+        int[][] testGrid = testBoard.getGrid();
         int count = 0;
 
         for(int row = 0; row < SIZE; row++) {
             for(int col = 0; col < SIZE; col++) {
-                if(testGrid[row][col].getNum() == 1) {
+                if(testGrid[row][col] == 1) {
                     count++;
                 }
             }
@@ -44,9 +40,9 @@ public class BoardTest {
     }
 
     @Test
-    void testGetTile() {
-        testBoard.getTile(0, 0).setNum(1);
-        testBoard.getTile(0, 1).setNum(2);
+    void testGetNum() {
+        testBoard.setNum(1, 0, 0);
+        testBoard.setNum(2, 0, 1);
         assertEquals("1 2 0 0 \n" +
                 "0 0 0 0 \n" +
                 "0 0 0 0 \n" +
@@ -55,8 +51,17 @@ public class BoardTest {
 
     @Test
     void testPushLeftWhenLeftIsEmpty() {
-        testBoard.getTile(0, 1).setNum(1);
-        testBoard.getTile(0, 2).setNum(2);
+        testBoard.setNum(1, 0, 1);
+        testBoard.setNum(2, 0, 2);
+        testBoard.pushLeft();
+        assertEquals("1 2 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n" +
+                "0 0 0 0 \n", testBoard.toString());
+
+        testBoard.initBoard();
+        testBoard.setNum(1, 0, 2);
+        testBoard.setNum(2, 0, 3);
         testBoard.pushLeft();
         assertEquals("1 2 0 0 \n" +
                 "0 0 0 0 \n" +
@@ -66,17 +71,8 @@ public class BoardTest {
 
     @Test
     void testPushLeftWhenMiddleIsEmpty() {
-        testBoard.getTile(0, 0).setNum(1);
-        testBoard.getTile(0, 3).setNum(2);
-        testBoard.pushLeft();
-        assertEquals("1 2 0 0 \n" +
-                "0 0 0 0 \n" +
-                "0 0 0 0 \n" +
-                "0 0 0 0 \n", testBoard.toString());
-
-        testBoard.clearBoard();
-        testBoard.getTile(0, 1).setNum(1);
-        testBoard.getTile(0, 3).setNum(2);
+        testBoard.setNum(1, 0, 1);
+        testBoard.setNum(2, 0, 3);
         testBoard.pushLeft();
         assertEquals("1 2 0 0 \n" +
                 "0 0 0 0 \n" +
@@ -86,8 +82,8 @@ public class BoardTest {
 
     @Test
     void testPushLeftWhenRightIsEmpty() {
-        testBoard.getTile(0, 1).setNum(1);
-        testBoard.getTile(0, 3).setNum(2);
+        testBoard.setNum(1, 0, 1);
+        testBoard.setNum(2, 0, 2);
         testBoard.pushLeft();
         assertEquals("1 2 0 0 \n" +
                 "0 0 0 0 \n" +
