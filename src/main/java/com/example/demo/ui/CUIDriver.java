7f2213d44b97f19e107dba9bb444041dd7ecf523
Ina Lee <inalee1208@gmail.com>
2023-07-25 01:45:09 -0700

    Edited startGame method in Board.java for it to start the game itself and handle input exceptions. Edited CUIDriver.java accordingly.

--- src/main/java/com/example/demo/ui/CUIDriver.java
+++ src/main/java/com/example/demo/ui/CUIDriver.java
@@ -2,29 +2,9 @@ package com.example.demo.ui;
 
 import com.example.demo.model.Board;
 
-import java.util.Scanner;
-
 public class CUIDriver {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         Board board = new Board();
         board.startGame();
-        boolean keepPlaying = true;
-
-        while(keepPlaying) {
-            System.out.println(board);
-            if(board.isGameOver()) {
-                break;
-            }
-            try {
-                char command = scan.nextLine().toUpperCase().charAt(0);
-                if(command == 'Q') {
-                    keepPlaying = false;
-                }
-                board.push(command);
-            } catch (StringIndexOutOfBoundsException e) {
-                System.out.println(e);
-            }
-        }
     }
 }
