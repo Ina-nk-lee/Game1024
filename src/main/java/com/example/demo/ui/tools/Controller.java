4ab248626748c75b37dcd224440b786d539b9ac6
Ina <matz1208@students.cs.ubc.ca>
2023-07-25 23:57:46 -0700

    Revised onKeyPress method to enhance encapsulation.

--- src/main/java/com/example/demo/ui/tools/Controller.java
+++ src/main/java/com/example/demo/ui/tools/Controller.java
@@ -1,34 +1,27 @@
 package com.example.demo.ui.tools;
 
-import com.example.demo.model.Board;
 import javafx.scene.input.KeyEvent;
 
 public class Controller {
-    private static Board board;
-
     private Controller() {
-    }
-
-    public static void setController(Board newBoard) {
-        board = newBoard;
     }
 
     public static void onKeyPress(KeyEvent e) {
         switch(e.getCode()) {
             case LEFT:
-                board.push('A');
+                BoardBuilder.push('A');
                 BoardBuilder.updateBoard();
                 break;
             case RIGHT:
-                board.push('D');
+                BoardBuilder.push('D');
                 BoardBuilder.updateBoard();
                 break;
             case UP:
-                board.push('W');
+                BoardBuilder.push('W');
                 BoardBuilder.updateBoard();
                 break;
             case DOWN:
-                board.push('S');
+                BoardBuilder.push('S');
                 BoardBuilder.updateBoard();
                 break;
             default:
