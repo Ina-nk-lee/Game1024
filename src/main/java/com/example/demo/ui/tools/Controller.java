5507723755b6f9f159f2ac9bdec0e553e36098f5
Ina Lee <inalee1208@gmail.com>
2023-07-25 23:50:45 -0700

    Added an event handler method to process user input(key press events).

--- src/main/java/com/example/demo/ui/tools/Controller.java
+++ src/main/java/com/example/demo/ui/tools/Controller.java
@@ -1,14 +1,38 @@
 package com.example.demo.ui.tools;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import com.example.demo.model.Board;
+import javafx.scene.input.KeyEvent;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+    private static Board board;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Controller() {
+    }
+
+    public static void setController(Board newBoard) {
+        board = newBoard;
+    }
+
+    public static void onKeyPress(KeyEvent e) {
+        switch(e.getCode()) {
+            case LEFT:
+                board.push('A');
+                BoardBuilder.updateBoard();
+                break;
+            case RIGHT:
+                board.push('D');
+                BoardBuilder.updateBoard();
+                break;
+            case UP:
+                board.push('W');
+                BoardBuilder.updateBoard();
+                break;
+            case DOWN:
+                board.push('S');
+                BoardBuilder.updateBoard();
+                break;
+            default:
+                break;
+        }
     }
 }
