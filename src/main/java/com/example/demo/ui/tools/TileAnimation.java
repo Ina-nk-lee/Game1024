1231af38193ce3e2d00d762897f6ed169944c3d7
Ina Lee <inalee1208@gmail.com>
2023-08-01 13:32:31 -0700

    Added animations for tiles to move more dynamically.

---
+++ src/main/java/com/example/demo/ui/tools/TileAnimation.java
@@ -0,0 +1,36 @@
+package com.example.demo.ui.tools;
+
+import com.example.demo.model.Tile;
+import javafx.animation.AnimationTimer;
+
+import java.util.ArrayList;
+
+public class TileAnimation extends AnimationTimer {
+    private final int SPACING = 5;
+
+    @Override
+    public void handle(long now) {
+        ArrayList<Tile> tiles = BoardBuilder.getTiles();
+
+        for(int i = 0; i < tiles.size(); i++) {
+            Tile cur = tiles.get(i);
+
+            if(cur.getLayoutX() + cur.getTranslateX() < 10) {
+                cur.slide.stop();
+                cur.setTranslateX(10 - cur.getLayoutX());
+            } else if(cur.getLayoutX() + cur.getTranslateX() + cur.getLength() > 225) {
+                cur.slide.stop();
+                cur.setTranslateX(225 - cur.getLayoutX() - cur.getLength());
+            }
+//                cur.setTranslateX(10 - cur.getLayoutX());
+//            } else if (i > 0){
+//                Tile other = tiles.get(i - 1);
+//                double tileDistance = cur.getLayoutX() - other.getLayoutX() + cur.getLength();
+//                if(tileDistance < SPACING) {
+//                    cur.slide.stop();
+//                    break;
+//                }
+//            }
+        }
+    }
+}
